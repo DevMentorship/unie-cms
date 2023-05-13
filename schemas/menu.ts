8c1f2a23ed0b053b6a98e0fafbdceb10a92050ab
Alexandr Naum <mfq07k9cz@gmail.com>
2023-05-13 23:08:12 +0300

    Update Sanity structure for Menu Page

--- schemas/menu.ts
+++ schemas/menu.ts
@@ -6,23 +6,73 @@ export default defineType({
   type: 'document',
   fields: [
     defineField({
-      name: 'mainImage',
-      title: 'Main image',
+      name: 'Poster',
+      title: 'Poster of dishes',
       type: 'image',
+      validation: (Rule) => Rule.required(),
       options: {
         hotspot: true,
       },
+      fields: [
+        {
+          name: 'caption',
+          type: 'string',
+          title: 'Caption',
+        },
+        {
+          name: 'attribution',
+          type: 'string',
+          title: 'Attribution',
+        },
+      ],
     }),
     defineField({
-      name: 'description',
-      title: 'Description',
+      name: 'dishName',
+      title: 'Dish name',
       type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'Weight',
+      title: 'food weight',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'Price',
+      title: 'price',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'dishDescription',
+      title: 'Dish description',
+      type: 'string',
+      validation: (Rule) => Rule.required().min(10).max(80),
+    }),
+    defineField({
+      name: 'proteins',
+      title: 'proteins',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'fats',
+      title: 'fats',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'carbohydrates',
+      title: 'carbohydrates',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'calories',
+      title: 'calories',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
     }),
   ],
-  preview: {
-    select: {
-      title: 'description',
-      media: 'mainImage',
-    },
-  },
 })
