db52f75babed2208f8a9d9eaa8140fa71b1902a1
Anton Potapov <panton160786@gmail.com>
2023-05-23 22:33:05 +0930

    new Schema 'news' has added

--- schemas/index.ts
+++ schemas/index.ts
@@ -4,5 +4,6 @@ import post from './post'
 import author from './author'
 import menu from './menu'
 import hero from './hero'
+import news from './news'
 
-export const schemaTypes = [post, author, category, blockContent, menu, hero]
+export const schemaTypes = [post, author, category, blockContent, menu, hero, news]
