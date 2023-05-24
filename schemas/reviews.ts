b3121813a383a30d7ad3bdf6bbd2ac73fad5ea0a
Liia Zubova <vardlitmo@gmail.com>
2023-05-24 17:39:44 +0300

    add: кумшуцы

--- schemas/reviews.ts
+++ schemas/reviews.ts
@@ -7,38 +7,8 @@ export default defineType({
   fields: [
     defineField({
       name: 'image',
-      title: 'Photo',
-      type: 'image',
-    }),
-    defineField({
-      name: 'name',
-      title: 'Name',
-      type: 'string',
-    }),
-    defineField({
-      name: 'review',
       title: 'Review',
-      type: 'string',
-    }),
-    defineField({
-      name: 'stars',
-      title: 'Stars',
-      type: 'string',
-    }),
-    defineField({
-      name: 'description',
-      title: 'Description',
-      type: 'text',
-    }),
-    defineField({
-      name: 'publishedDate',
-      title: 'Date',
-      type: 'date',
-    }),
-    defineField({
-      name: 'like',
-      title: 'Like',
       type: 'image',
-    }),
+    })
   ],
 })
